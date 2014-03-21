484599bc7d63fec97600f011dbbe71adc8f0e95d
jerrybarolo <gfiorletta@gmail.com>
2014-03-21 17:07:24 +0100

    Create LetterCountI.js
    Have the function LetterCountI(str) take the str parameter being passed and return the first word with the greatest number of repeated letters. For example: "Today, is the greatest day ever!" should return greatest because it has 2 e's (and 2 t's) and it comes before ever which also has 2 e's. If there are no words with repeating letters return -1. Words will be separated by spaces.

---
+++ LetterCountI.js
@@ -0,0 +1,37 @@
+function countMax(str){
+    str=str.toLowerCase();
+    var arr={};
+    for(i=0; i < str.length; i++)
+    {
+        var char=str.charAt(i);
+        if (arr[char]) 
+          arr[char]++;
+        else 
+          arr[char]=1;
+    }
+  
+    var max=0;
+    for (var k in arr)
+    {
+      if (arr[k]>max) 
+        max=arr[k];
+    }
+  
+  return max;
+}
+
+function LetterCountI(str) {
+  str=str.split(" ");
+  var max=0, maxi=0;
+  for (var i=0;i<str.length;i++)
+  {
+     var total=countMax(str[i]);
+     if (total>max)
+     {
+       max=total;
+       maxi=i;
+     }
+  }
+  if(max>1) return str[maxi];
+  return -1;
+}
