300c0965e69624150edf0348ab289075697f3a23
jerrybarolo <gfiorletta@gmail.com>
2014-03-25 16:10:06 +0100

    Create FormattedDivision.js
    Have the function FormattedDivision(num1,num2) take both parameters being passed, divide num1 by num2, and return the result as a string with properly formatted commas and 4 significant digits after the decimal place. For example: if num1 is 123456789 and num2 is 10000 the output should be "12,345.6789". The output must contain a number in the one's place even if it is a zero.

---
+++ Medium/FormattedDivision.js
@@ -0,0 +1,15 @@
+function number_format( number, decimals, dec_point, thousands_sep ) 
+{    
+    var n = number, c = isNaN(decimals = Math.abs(decimals)) ? 2 : decimals;
+    var d = dec_point == undefined ? "." : dec_point;
+    var t = thousands_sep == undefined ? "," : thousands_sep, s = n < 0 ? "-" : "";
+    var i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "", j = (j = i.length) > 3 ? j % 3 : 0;
+    
+    return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+}
+
+function FormattedDivision(num1,num2) { 
+
+  var res= num1/num2;
+  return number_format(res,4,".",",");
+}
