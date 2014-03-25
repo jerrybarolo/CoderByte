d1bc4013afbd9c848b2573e78b6fb40fa45f4324
jerrybarolo <gfiorletta@gmail.com>
2014-03-25 16:04:28 +0100

    Create ArrayAddition.js
    Have the function ArrayAddition(arr) take the array of numbers stored in arr and return the string true if any combination of numbers in the array can be added up to equal the largest number in the array, otherwise return the string false. For example: if arr contains [4, 6, 23, 10, 1, 3] the output should return true because 4 + 6 + 10 + 3 = 23. The array will not be empty, will not contain all the same elements, and may contain negative numbers.

---
+++ Medium/ArrayAddition.js
@@ -0,0 +1,19 @@
+function findSubSum( target , a )
+{
+  if( a.length == 1 )
+    return (a[0] == target);
+  if( a[0] == target )
+    return true;
+  var i = a.shift();
+  if( i > target )
+    return findSubSum( target , a.slice(0) );
+  else
+    return findSubSum( target , a.slice(0) ) || findSubSum( target - i , a.slice(0) );
+}
+ 
+function ArrayAddition(arr)
+{ 
+  arr = arr.sort(function(a,b){return a -b;});
+  var  target = arr.pop();
+  return findSubSum( target , arr );
+}
